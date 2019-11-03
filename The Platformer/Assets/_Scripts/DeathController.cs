295ddcd287faaebc8a882c49454f646135957f96
Exzeed <geethan1996@hotmail.ca>
2019-11-03 18:28:49 -0500

    Tile Maps
    created tile map grid and foreground and background tile layers

---
+++ The Platformer/Assets/_Scripts/DeathController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 
+/// </summary>
+[System.Serializable]
+public class DeathController : MonoBehaviour
+{
+    public Transform activeSpawnpoint;
+    public GameObject player;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.position = activeSpawnpoint.position;
+        }
+    }
+}
