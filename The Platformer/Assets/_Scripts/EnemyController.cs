17bec8b39df539b24568d6efcfca7cdbd86251b3
Exzeed <geethan1996@hotmail.ca>
2019-11-03 11:43:18 -0500

    Enemy related
    Updated enemy game object, controller and animation controller

--- The Platformer/Assets/_Scripts/EnemyController.cs
+++ The Platformer/Assets/_Scripts/EnemyController.cs
@@ -1,18 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Util;
+/// <summary>
+/// 
+/// </summary>
 public class EnemyController : MonoBehaviour
 {
+    public EnemyState enemyState;
+    public Animator enemyAnimator;
+    public SpriteRenderer enemySpriteRenderer;
+    public Rigidbody2D enemyRigidBody;
+    public bool isGrounded;
+    public bool hasGroundAhead;
+    public bool hasWallAhead;
+    public Transform lookAhead;
+    public Transform wallAhead;
+    public bool isFacingRight = true;
+    public float movementSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyState = EnemyState.WALK;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        isGrounded = Physics2D.BoxCast(
+            transform.position, new Vector2(2.0f, 1.0f), 0.0f, Vector2.down, 1.0f, 1 << LayerMask.NameToLayer("Ground"));
+
+        hasGroundAhead = Physics2D.Linecast(
+            transform.position,
+            lookAhead.position,
+            1 << LayerMask.NameToLayer("Ground"));
+
+        hasWallAhead = Physics2D.Linecast(
+            transform.position,
+            wallAhead.position,
+            1 << LayerMask.NameToLayer("Ground"));
+
+        if (isGrounded)
+        {
+            if (isFacingRight)
+            {
+                enemyRigidBody.velocity = new Vector2(movementSpeed, 0.0f);
+            }
+
+            if (!isFacingRight)
+            {
+                enemyRigidBody.velocity = new Vector2(-movementSpeed, 0.0f);
+            }
+
+            if (!hasGroundAhead || hasWallAhead)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, 1.0f, 1.0f);
+                isFacingRight = !isFacingRight;
+            }
+        }
     }
 }
